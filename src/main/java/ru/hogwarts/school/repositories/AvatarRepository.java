8300face6fa9aa580cecffa3cf89ac95057732b0
ZR059 <zubairov_90@mail.ru>
2025-10-15 22:19:29 +0500

    сделано дз

--- src/main/java/ru/hogwarts/school/repositories/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/repositories/AvatarRepository.java
@@ -2,12 +2,16 @@ package ru.hogwarts.school.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Avatar;
 
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
 
     Optional<Avatar> findByStudentId(Long studentId);
+
+    Page<Avatar> findAll(Pageable pageable);
 
 }
 
