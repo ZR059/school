d8c4796847938e9ccfab449f778ff2ac1568172a
ZR059 <zubairov_90@mail.ru>
2025-10-20 22:03:13 +0500

    сделано дз

---
+++ scripts422.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Car (
+    id SERIAL PRIMARY KEY,
+    brand VARCHAR(100) NOT NULL,
+    model VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE Person (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INTEGER NOT NULL,
+    has_license BOOLEAN NOT NULL DEFAULT FALSE,
+    car_id INTEGER,
+    FOREIGN KEY (car_id) REFERENCES Car(id)
+);
